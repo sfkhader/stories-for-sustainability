6ab1be92d2f20844115ae8511e72fae33a011465
Hye Jin Lim <limhj098@gmail.com>
2019-09-07 13:18:06 -0400

    Got rid of the header for navigation

--- webapp/stories-for-sustainability/react-firebase-authentication/src/components/App/index.js
+++ webapp/stories-for-sustainability/react-firebase-authentication/src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navigation from '../Navigation';
+// import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
@@ -13,8 +13,8 @@ import { withAuthentication } from '../Session';
 const App = () => (
   <Router>
     <div>
-      <Navigation /> 
-      <hr /> 
+      {/* <Navigation />  */}
+      {/* <hr />  */}
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
