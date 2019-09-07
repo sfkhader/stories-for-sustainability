909be85c11f95b53ddede139ca9a38060c616ad8
Rwan <rhayek3@gatech.edu>
2019-09-07 02:22:03 -0400

    changed ui of landing page

--- webapp/stories-for-sustainability/react-firebase-authentication/src/components/Landing/index.js
+++ webapp/stories-for-sustainability/react-firebase-authentication/src/components/Landing/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
 import logo from '../../logo2.png';
 import { makeStyles } from '@material-ui/core/styles';
 import '../../App.css';
+import Button from '@material-ui/core/Button';
+import React, { Component } from 'react';
+import * as ROUTES from '../../constants/routes';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+
+
 const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-    <img src={logo} className="App-logo" alt="logo" />
+  <div className="Landing">
+
+    <h1 className = "Landing-header">
+    <img src={logo} className="Landing-logo" alt="logo" />
+
+      Stories for Sustainability 
+      
+      <Link to={ROUTES.SIGN_UP}> 
+      <button class="button" >
+        Register
+      </button>
+      </Link>
+      
+
+    </h1>
 
   </div>
 );
