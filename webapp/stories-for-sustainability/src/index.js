34aeae6ab7426f66e40f7a0fd6cc531ec4062ea7
Hye Jin Lim <limhj098@gmail.com>
2019-09-07 08:50:46 -0400

    Started firebase

--- webapp/stories-for-sustainability/src/index.js
+++ webapp/stories-for-sustainability/src/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import { render } from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from 'react-dom';
 
-// import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Register from "./Register";
-import Container from "./Container";
 import * as serviceWorker from './serviceWorker';
 
-const App = () => (
-  <Router>
-    <div>
-      {/* <App /> */}
-      {/* <Register /> */}
-      <Container />
-    </div>
-  </Router>
-);
+import App from './components/App';
+import Firebase, { FirebaseContext } from '../src/components/Firebase';
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  
+  document.getElementById('root'));
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
