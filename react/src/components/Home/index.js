a19f3adb66513633ae630c8f1345793ac001d660
Samia Khan <skhan340@gatech.edu>
2019-09-11 11:50:10 -0400

    master is up to date

---
+++ react/src/components/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../logo2.png';
+
+const Home = () => (
+  <div className="Landing-header">
+    <img src={logo} className="Landing-logo" alt="logo" />
+    <h1>Welcome!</h1>
+  </div>
+);
+export default Home;
---
+++ react/src/components/Landing/index.js
@@ -0,0 +1,36 @@
+import logo from '../../logo2.png';
+import '../../App.css';
+import React, { Component } from 'react';
+import * as ROUTES from '../../constants/routes';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+
+
+const Landing = () => (
+  <div className="Landing">
+
+    <header className = "Landing-header">
+      <img src={logo} className="Landing-logo" alt="logo" />
+
+        Stories for Sustainability 
+        
+        <Link to={ROUTES.SIGN_UP}> 
+        <button class="button" >
+          Register
+        </button>
+        </Link>
+        
+        <Link to={ROUTES.SIGN_IN}> 
+        <button class="login-button" >
+          Log In
+        </button>
+        </Link>
+
+      
+
+    </header>
+
+  </div>
+);
+export default Landing;
